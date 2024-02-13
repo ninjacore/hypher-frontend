b4a90f23cc938bad055ac55433dbaa52d409c558
zimtz <zer0d1ve@tuta.io>
2024-02-13 14:04:45 +0000

    res.json .then resolves too early (no data)

--- src/app/tmp/[handle]/seven/page.jsx
+++ src/app/tmp/[handle]/seven/page.jsx
@@ -1,5 +1,6 @@
 "use client"
 import { useContext, useEffect, useState } from "react"
+import splitProfileData from "./splitProfileData"
 
 export default function Page() {
   return (
@@ -18,11 +19,31 @@ function ValueOfInterest() {
 
   // to make sure state is updated once we get the data pt.2
   useEffect(() => {
-    fetch("http://localhost:5678/api/v1/profilePage/dnt.is").then(() =>
-      setLoaded(true)
-    )
-    // setLoaded(true)
-    value = "ab"
+    // fetch("http://localhost:5678/api/v1/profilePage/dnt.is").then(() =>
+    //   setLoaded(true)
+    // )
+    fetch("http://localhost:5678/api/v1/profilePage/dnt.is")
+      .then((res) => {
+        if (!res.status.toString().startsWith("2")) {
+          throw new Error(`HTTP error! status: ${res.status}`)
+        }
+        setLoaded(true)
+        console.log("res=")
+        console.log(res)
+        // promiese to decode the response body as json
+        return res.json()
+      })
+      // once deconstructed, we can use the data
+      .then((decodedResponse) => {
+        console.log("decodedResponse=")
+        console.log(decodedResponse)
+      })
+      .then((profileData) => {
+        // split profile data to make it usable
+        let splitData = splitProfileData(profileData)
+        console.log("splitData=")
+        console.log(splitData)
+      })
     setData(value)
   }, [])
 
