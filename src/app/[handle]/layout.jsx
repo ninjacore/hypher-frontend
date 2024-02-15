af13b52513b3b31b370b0906af02189371cccbab
zimtz <zer0d1ve@tuta.io>
2024-02-15 13:25:50 +0000

    show edit symbol for main

--- src/app/[handle]/layout.jsx
+++ src/app/[handle]/layout.jsx
@@ -10,10 +10,16 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
-const title = "Parallel Routes"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faAngleRight, faPenToSquare } from "@fortawesome/free-solid-svg-icons"
+
+const title = "Profile Page"
 
 // to be handled by token
 const pageOwner = true
+
+// to be handley by button click
+const editMode = true
 
 export const metadata = {
   title,
@@ -39,6 +45,20 @@ function ProfilePageButton({ isOwner }) {
   )
 }
 
+function EditButton() {
+  return (
+    <Button
+      // variant="default"
+      className="group-hover/edit:bg-accent group-hover/edit:text-accent-foreground text-black rounded p-0.5"
+    >
+      <FontAwesomeIcon
+        icon={faPenToSquare}
+        className="fas fa-pen-to-square text-xs px-1.5 my-auto my-2.45 ml-1 py-0"
+      ></FontAwesomeIcon>
+    </Button>
+  )
+}
+
 export default function Layout({
   name,
   about,
@@ -46,6 +66,40 @@ export default function Layout({
   linkCollection,
   featured,
 }) {
+  // edit mode
+  if (editMode) {
+    return (
+      <>
+        <Card className="mb-2">
+          <CardSingleLineHeader className="flex justify-between my-4">
+            <h1 className="text-3xl font-bold mx-1">{name}</h1>
+          </CardSingleLineHeader>
+          <CardContent>
+            <div className="px-1 py-2 group/edit">
+              {about}
+              <EditButton />
+            </div>
+
+            <div className="mt-2 px-1">
+              <hr className="border-gray-400 border-t-2" />
+            </div>
+            <div className="mt-2 group/edit">
+              {tags}
+              <EditButton />
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="mb-6">
+          <CardContent>{linkCollection}</CardContent>
+        </Card>
+        <Card className="mb-6">
+          <CardContent>{featured}</CardContent>
+        </Card>
+      </>
+    )
+  }
+
+  // view mode
   return (
     <>
       <Card className="mb-2">
