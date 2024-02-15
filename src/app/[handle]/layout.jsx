6907c5ab32011d2d713e3ddf3926ff55ef6ed312
zimtz <zer0d1ve@tuta.io>
2024-02-15 11:24:03 +0000

    ownership layout activated

--- src/app/[handle]/layout.jsx
+++ src/app/[handle]/layout.jsx
@@ -12,12 +12,31 @@ import {
 
 const title = "Parallel Routes"
 
+// to be handled by token
+const pageOwner = true
+
 export const metadata = {
   title,
   openGraph: {
     title,
     images: [`/api/og?title=${title}`],
   },
+}
+
+function ProfilePageButton({ isOwner }) {
+  if (isOwner) {
+    return (
+      <Button variant="outline" className="bg-white text-black">
+        {"edit"}
+      </Button>
+    )
+  }
+
+  return (
+    <Button variant="outline" className="bg-white text-black">
+      {"connect"}
+    </Button>
+  )
 }
 
 export default function Layout({
@@ -32,9 +51,7 @@ export default function Layout({
       <Card className="mb-2">
         <CardSingleLineHeader className="flex justify-between my-4">
           <h1 className="text-3xl font-bold mx-1">{name}</h1>
-          <Button variant="outline" className="bg-white text-black">
-            connect
-          </Button>
+          <ProfilePageButton isOwner={pageOwner} />
         </CardSingleLineHeader>
         <CardContent>
           <div className="px-1 py-2">{about}</div>
