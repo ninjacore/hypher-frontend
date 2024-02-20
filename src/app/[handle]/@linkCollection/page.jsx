ba5dc4e98f7a1fa35b2defa980aa3cb6527941db
zimtz <zer0d1ve@tuta.io>
2024-02-20 09:22:21 +0000

    links always open new tab

--- src/app/[handle]/@linkCollection/page.jsx
+++ src/app/[handle]/@linkCollection/page.jsx
@@ -33,7 +33,7 @@ function LinkCollection() {
         key={"pos-" + link.position}
         className="my-4 mx-2 py-2 px-3 bg-konkikyou-blue"
       >
-        <a href={link.url}>
+        <a href={link.url} target="_blank">
           <IconMapper url={link.url} />
           <span className="mx-2">
             {link.text.length > 0 ? link.text : link.url}
