8f483b57f9b14b7d2483de441b14b016db1febb0
zimtz <zer0d1ve@tuta.io>
2024-02-21 10:24:38 +0000

    adaptable input fields via states

--- src/app/[handle]/@editableFeatured/page.jsx
+++ src/app/[handle]/@editableFeatured/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useContext } from "react"
+import { useContext, useState } from "react"
 // import { ProfileContext, Profile } from "../ProfileProvider"
 import { ProfileContext, Profile } from "../page.jsx"
 import { IconMapper } from "../../../components/iconMapper"
@@ -42,78 +42,86 @@ function EditableFeatured() {
   console.log("featuredContent")
   console.log(featuredContent)
 
+  // for the input fields
+  const [featuredTitle, setFeaturedTitle] = useState("")
+  const [featuredLink, setFeaturedLink] = useState("")
+  const [featuredDescription, setFeaturedDescription] = useState("")
+
   let title = featuredContent.shortTitle
   const innerHTML = featuredContent.contentBox.map((contentBox) => {
     return (
       <Card key={contentBox.category + contentBox.position} className="my-4">
         {/* <a onClick={(e) => editLink(e, 2)} id={contentBox.position + "x"}> */}
-        <a>
-          <Dialog>
-            <DialogTrigger asChild>
-              <div className="flex group/edit">
-                <div className="text-5xl py-4 px-2">
-                  <IconMapper url={contentBox.category + ":"} />
-                </div>
-                <div className="grow p-2">
-                  <span>
-                    {contentBox.title.length > 0 ? contentBox.title : ""}
-                  </span>
-                  <EditButton />
-                  <br />
-                  <span>
-                    {contentBox.description.length > 0
-                      ? contentBox.description
-                      : contentBox.url}
-                  </span>
-                </div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <div className="flex group/edit">
+              <div className="text-5xl py-4 px-2">
+                <IconMapper url={contentBox.category + ":"} />
               </div>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-              <DialogHeader>
-                <DialogTitle>Edit Content</DialogTitle>
-                <DialogDescription>
-                  Make changes to your content here. Click save when you're
-                  done.
-                </DialogDescription>
-              </DialogHeader>
-              <div className="grid gap-4 py-4">
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="featuredTitle" className="text-right">
-                    Title
-                  </Label>
-                  <Input
-                    id="featuredTitle"
-                    value="Metal Rock Radio"
-                    className="col-span-3"
-                  />
-                </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="featuredLink" className="text-right">
-                    Link
-                  </Label>
-                  <Input
-                    id="featuredLink"
-                    value="https://open.spotify.com/dhkalsrhe5zu"
-                    className="col-span-3"
-                  />
-                </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="featuredDescription" className="text-right">
-                    Description
-                  </Label>
-                  <Input
-                    id="featuredDescription"
-                    value="..."
-                    className="col-span-3"
-                  />
-                </div>
+              <div className="grow p-2">
+                <span>
+                  {contentBox.title.length > 0 ? contentBox.title : ""}
+                </span>
+                <EditButton />
+                <br />
+                <span>
+                  {contentBox.description.length > 0
+                    ? contentBox.description
+                    : contentBox.url}
+                </span>
               </div>
-              <DialogFooter>
-                <Button type="submit">Save changes</Button>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
-        </a>
+            </div>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit Content</DialogTitle>
+              <DialogDescription>
+                Make changes to your content here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="featuredTitle" className="text-right">
+                  Title
+                </Label>
+                <Input
+                  id="featuredTitle"
+                  type="text"
+                  className="col-span-3"
+                  onChange={(e) => setFeaturedTitle(e.target.value)}
+                  value={featuredTitle}
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="featuredLink" className="text-right">
+                  Link
+                </Label>
+                <Input
+                  id="featuredLink"
+                  type="text"
+                  className="col-span-3"
+                  onChange={(e) => setFeaturedLink(e.target.value)}
+                  value={featuredLink}
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="featuredDescription" className="text-right">
+                  Description
+                </Label>
+                <Input
+                  id="featuredDescription"
+                  type="text"
+                  className="col-span-3"
+                  onChange={(e) => setFeaturedDescription(e.target.value)}
+                  value={featuredDescription}
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </Card>
     )
   }, [])
