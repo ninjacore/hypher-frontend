986adb69f3b5e7716ac1a539abdf519be1de6ec7
zimtz <zer0d1ve@tuta.io>
2024-03-06 10:13:19 +0000

    tags buffer from input field

--- src/app/profile/tags/edit/page.jsx
+++ src/app/profile/tags/edit/page.jsx
@@ -1,10 +1,22 @@
 "use client"
-import React, { createContext, useContext, useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Separator } from "@/components/ui/separator"
 import { Progress } from "@/components/ui/progress"
 import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog"
 
 export default function Page() {
   return (
@@ -17,6 +29,8 @@ export default function Page() {
 export function TagEditor({ children }) {
   const [tags, setTags] = useState(null)
   const [loaded, setLoaded] = useState(false)
+
+  const [newTagsBuffer, setNewTagsBuffer] = useState(null)
 
   // get user tag by serving endpoint and having it as a query parameter
   let handle = "dnt.is"
@@ -70,35 +84,87 @@ export function TagEditor({ children }) {
   //     <TagScrollArea tags={tagsArray} />
   //   </>
   // )
+
+  const innerHTML = tagsArray.map((tag) => {
+    return (
+      <>
+        <span
+          key={"tag-" + tagCount++}
+          className="inline-flex mx-1.5 my-1 px-3 py-0.45 rounded text-sm font-medium bg-white text-black"
+        >
+          {tag}
+        </span>
+      </>
+    )
+  }, [])
+
+  let tagText = ""
+
   return (
     <div className="mx-1">
       <Progress value={progress} className="w-[60%] my-4" />
 
       <h2 className="mb-2">
         <strong>Your Tags</strong>
-      </h2>
-      <div>
-        {tagsArray.map((tag) => (
-          <span
-            key={"tag-" + tagCount++}
-            className="inline-flex mx-1.5 my-1 px-3 py-0.45 rounded text-sm font-medium bg-white text-black"
-          >
-            {tag}
-          </span>
-        ))}
-      </div>
-      <div className="mt-5 flex justify-end">
-        <div className="w-2/4 mt-2 mr-6 flex justify-end gap-5">
-          <Button variant="outline" className="bg-white text-black">
-            REORDER
-          </Button>
-          <Button variant="outline" className="bg-white text-black">
-            ADD
-          </Button>
+        <div>{innerHTML}</div>
+        <div className="mt-5 flex justify-end">
+          <div className="w-2/4 mt-2 mr-6 flex justify-end gap-5">
+            <Button variant="outline" className="bg-white text-black">
+              REORDER
+            </Button>
+            <Dialog>
+              <DialogTrigger asChild>
+                <div className="group/edit">
+                  <Button variant="outline" className="bg-white text-black">
+                    ADD
+                  </Button>
+                </div>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Edit Content</DialogTitle>
+                  <DialogDescription>
+                    Tags will help you find other people with similar interests.
+                    Click save when you're done.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="featuredTitle" className="text-right">
+                      Title
+                    </Label>
+                    <Input
+                      id="tagsInput"
+                      type="text"
+                      className="col-span-3"
+                      onChange={(e) => setNewTagsBuffer(e.target.value)}
+                      //value={tagText}
+                    />
+                  </div>
+                </div>
+                <DialogFooter>
+                  <DialogClose>
+                    <Button
+                      type="submit"
+                      onClick={(e) => {
+                        updateTagText(newTagsBuffer)
+                      }}
+                    >
+                      Save
+                    </Button>
+                  </DialogClose>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          </div>
         </div>
-      </div>
+      </h2>
     </div>
   )
+}
+
+function updateTagText(newTagsBuffer) {
+  console.log("updateTagText => " + newTagsBuffer)
 }
 
 function TagScrollArea({ tags }) {
