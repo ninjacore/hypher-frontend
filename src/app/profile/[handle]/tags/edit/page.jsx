5e33cc1c00b91536699a716f3360ac0737a1367a
zimtz <zer0d1ve@tuta.io>
2024-03-18 16:24:23 +0000

    typo: shoudl be 'tag'

--- src/app/profile/[handle]/tags/edit/page.jsx
+++ src/app/profile/[handle]/tags/edit/page.jsx
@@ -255,7 +255,7 @@ export function TagEditModeButtons({
           <div className="grid gap-4 py-4">
             <div className="grid grid-cols-4 items-center gap-4">
               <Label htmlFor="featuredTitle" className="text-right">
-                Title
+                Tag
               </Label>
               <Input id="tagsInput" type="text" className="col-span-3" />
             </div>
