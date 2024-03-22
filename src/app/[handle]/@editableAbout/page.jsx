7b3c40f873e8129fd9f1890532bb9468d2dab8f4
zimtz <zer0d1ve@tuta.io>
2024-03-22 11:12:09 +0000

    update sent to database

--- src/app/[handle]/@editableAbout/page.jsx
+++ src/app/[handle]/@editableAbout/page.jsx
@@ -71,8 +71,34 @@ function saveUpdatedBio() {
   let textBuffer = document.getElementById("aboutText").value
   console.log("updated bio: ", textBuffer)
 
-  // update context (so change shows for user)
+  // TODO: update context (so change shows for user)
 
   // save to database
   // {{SERVICE_IP}}:{{SERVICE_PORT}}/{{API_VERSION}}/profiles/dnt.is/about
+  // get handle from url of this page
+  const url = window.location.href
+  const handle = url.split("/").pop()
+  console.log("handle=")
+  console.log(handle)
+
+  const apiURL = `http://localhost:5678/api/v1/profiles/${handle}/about`
+
+  fetch(apiURL, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      bio: textBuffer,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data)
+      return data
+    })
+    .catch((error) => {
+      console.error("Error:", error)
+      return error
+    })
 }
