527d76f5f74336976ba6d1fafc1d4e0391d06c20
zimtz <zer0d1ve@tuta.io>
2024-04-17 15:19:50 +0100

    finally — handle via context API to Redux

--- src/app/[handle]/page.jsx
+++ src/app/[handle]/page.jsx
@@ -4,6 +4,7 @@ import React, { createContext, useEffect, useState } from "react"
 // to derive handle
 import { usePathname } from "next/navigation"
 import { deriveProfileHandle } from "@/lib/utils/profileHandleDeriver"
+import path from "path"
 
 // creating context to use later and in child components
 export const ProfileContext = createContext(null)
@@ -18,47 +19,31 @@ export default function Page() {
 
 export function Profile({ children }) {
   const [handle, setHandle] = useState(null)
-  // let handle = null
-  const pathname = usePathname()
-
-  // const [pathname, setPathname] = useState(null)
-  // setPathname(usePathname())
 
   // get handle from url of this page
-  // const pathname =
-  //   const handle = deriveProfileHandle(pathname)
+  const pathname = usePathname()
 
   // to limit re-renders
   useEffect(() => {
-    console.log("pathname is set to: ", pathname)
-    console.log("pathname.split('/').pop(1) = ", pathname.split("/").pop(1))
-    // setHandle(pathname.split("/").pop(1))
-    // handle = pathname.split("/").pop(1)
-    setHandle(pathname.split("/").pop(1))
-
-    // setHandle(deriveProfileHandle(pathname))
-    console.log("handle is set to: ", handle)
-  }, [pathname])
+    setHandle(pathname.split("/").pop())
+  }, [])
 
   //   setHanlde(deriveProfileHandle(pathname))
 
-  // if (handle) {
-  //   console.log("if (handle) TRUE: ", handle)
-  //   return (
-  //     <>
-  //       <ProfileContext.Provider value={handle}>
-  //         {children}
-  //       </ProfileContext.Provider>
-  //     </>
-  //   )
-  // }
+  if (handle) {
+    return (
+      <>
+        <ProfileContext.Provider value={handle}>
+          {children}
+        </ProfileContext.Provider>
+      </>
+    )
+  }
 
   // default
   return (
     <>
-      <ProfileContext.Provider value={handle}>
-        {children}
-      </ProfileContext.Provider>
+      <span>Loading..</span>
     </>
   )
 }
