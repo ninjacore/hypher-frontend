54d410c7f07b417a575e84037e1305c7dc6d885a
zimtz <zer0d1ve@tuta.io>
2024-05-17 11:40:05 +0100

    burger icon when dragging

--- src/lib/utils/SortableLinkNode/SortableLinkNode.jsx
+++ src/lib/utils/SortableLinkNode/SortableLinkNode.jsx
@@ -6,6 +6,9 @@ import { useSortable } from "@dnd-kit/sortable"
 import { CSS } from "@dnd-kit/utilities"
 import { IconMapper } from "@/components/iconMapper"
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGripLines } from "@fortawesome/free-solid-svg-icons"
+
 function SortableLinkNode(props) {
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: props.id })
@@ -27,12 +30,21 @@ function SortableLinkNode(props) {
     >
       <div
         key={"pos-" + props.position}
-        className="my-4 mx-2 py-2 px-3 bg-konkikyou-blue"
+        className="my-4 mx-2 py-2 px-3 bg-konkikyou-blue 
+        flex justify-between"
       >
-        <IconMapper url={props.url} key={"icon-" + props.id} />
-        <span className="mx-2" key={"contentOf-" + props.id}>
-          {props.text.length > 0 ? props.text : props.url}
-        </span>
+        <div className="">
+          <IconMapper url={props.url} key={"icon-" + props.id} />
+          <span className="mx-2" key={"contentOf-" + props.id}>
+            {props.text.length > 0 ? props.text : props.url}
+          </span>
+        </div>
+        <div className="pr-3">
+          <FontAwesomeIcon
+            icon={faGripLines}
+            className="fas fa-grip-lines text-base"
+          ></FontAwesomeIcon>
+        </div>
       </div>
     </div>
   )
