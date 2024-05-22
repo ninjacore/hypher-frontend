162b2e6c221f1a317906864943f791a63e94bcf1
zimtz <zer0d1ve@tuta.io>
2024-05-22 11:55:45 +0100

    apiHandler as its own module

--- src/lib/utils/profileDataClients/linkCollectionClient.js
+++ src/lib/utils/profileDataClients/linkCollectionClient.js
@@ -1,6 +1,6 @@
-import { create } from "domain"
 import { announce } from "../debugTools/announce"
 
+import { apiHandler } from "../apiHandler"
 const baseURL = "http://localhost:5678"
 
 export async function linkCollectionClient(
@@ -60,46 +60,6 @@ export async function linkCollectionClient(
 
     default:
       return await getLinkCollection(handle, contentBoxPosition, method)
-  }
-}
-
-async function apiHandler(endpoint, method, body = null) {
-  const config = {
-    method: method,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }
-
-  // anything but 'GET' and 'DELETE' usually has a body
-  if (body) {
-    config.body = JSON.stringify(body)
-  } else if (method === "PUT" || method === "POST") {
-    throw new Error("Body is required for ADD or UPATE.")
-  }
-
-  let data = null
-
-  try {
-    const response = await window.fetch(endpoint, config)
-    data = await response.json()
-    if (response.ok) {
-      return {
-        status: response.status,
-        data,
-        headers: response.headers,
-        url: response.url,
-      }
-    }
-    // if not ok..
-    throw new Error(response.statusText)
-  } catch (error) {
-    // temp for mobile debugging
-    document.getElementById("mobileMessageOutput").innerHTML = JSON.stringify(
-      error.message ? error.message : data
-    )
-
-    return Promise.reject(error.message ? error.message : data)
   }
 }
 
